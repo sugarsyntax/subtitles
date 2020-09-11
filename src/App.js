735fc651763fe17a5951eb2f6908267dfc7c05a1
Jackie Beltran <jacquelline500@gmail.com>
2020-09-11 08:39:25 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this)
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>	
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default VideoPlayer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import VideoPlayer from './App';
 import * as serviceWorker from './serviceWorker';
+import office from './assets/office.mp4'
+import cap from './assets/my_captions.vtt'
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [{
+    src: office,
+    type: 'video/mp4'
+  }],
+  track: [{
+    kind: 'captions',
+    src: cap,
+    srclang: 'en',
+    label: 'english'
+  }],
+  mode: 'showing'
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <VideoPlayer {...videoJsOptions}
+      width="720"
+      height="600"
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
